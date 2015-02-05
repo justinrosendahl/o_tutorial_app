6e59237d920ca8fed16d7379b16850ce6025845a
Justin Rosendahl <justin.rosendahl@gmail.com>
2015-02-04 20:19:20 -0600

    temp

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ OTutorialApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   resources :articles
-   root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
