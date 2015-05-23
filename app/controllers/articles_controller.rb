3f4471f486891c253f18e1a0c270569825a9ec80
Jennifer Rosendahl <jrimac@Jennifers-iMac.local>
2015-05-22 19:14:08 -0500

    5.10

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,14 +6,17 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 	def new
-
+		@article = Article.new
 	end
 	def create
-    #render plain: params[:article].inspect
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
-end
+    	#render plain: params[:article].inspect
+   		@article = Article.new(article_params)
+    	if @article.save
+    		redirect_to @article
+    	else
+    		render 'new'
+    	end
+	end
 
 private
 def article_params
